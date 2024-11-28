8ae029295822254321bf7d1ef06c5a6378aefa8b
AimanFadillah <mlakece914@gmail.com>
2024-11-28 16:54:16 +0700

    vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue()],
+  output: 'server',
+  adapter: vercel()
+});
---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p>Angka: {{ angka }}</p>
+    <button @click="tambahAngka">Tambah</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      angka: 0,
+    };
+  },
+  methods: {
+    tambahAngka() {
+      this.angka++;
+    },
+  },
+};
+</script>
